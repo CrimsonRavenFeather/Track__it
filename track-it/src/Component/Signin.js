2ba38a75b4e665a577d9aee9b1323fe9ebbfbfe7
CrimsonRavenFeather <sahil.crj8@gamil.com>
2024-09-29 20:02:02 +0530

    added loading animation in signin route

--- track-it/src/Component/Signin.js
+++ track-it/src/Component/Signin.js
@@ -1,16 +1,42 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../Context/Context'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(useGSAP)
+
 
 export const Signin = () => {
     const userState = useContext(UserContext)
+    const [loading, setLoading] = useState(false)
     const [email, setEmail] = useState('')
     const [pwd, setPwd] = useState('')
     const [name, setName] = useState('')
     const navigate = useNavigate()
 
+    const loaderRef = useRef(null)
+    const animationRef = useRef(null);
+
+    const LoaderAnimation = () => {
+        animationRef.current = gsap.to(loaderRef.current, {
+            rotation: 360,
+            duration: 1.5,
+            ease: "linear",
+            repeat: -1, // Infinite repeat
+        });
+    }
+
+    const KillLoaderAnimation = () => {
+        if (animationRef.current) {
+            animationRef.current.kill(); // Stop the GSAP animation
+        }
+    }
+
     const handleSignIn = async () => {
         try {
+            setLoading(true)
+            LoaderAnimation()
             const response = await fetch('https://track-it-backend-crimsonravens-projects.vercel.app/signin', {
                 method: 'POST',
                 headers: {
@@ -30,6 +56,8 @@ export const Signin = () => {
                 navigate('/login')
             }
             else {
+                setLoading(false)
+                KillLoaderAnimation()
                 console.log('Error', result)
             }
         } catch (error) {
@@ -37,20 +65,19 @@ export const Signin = () => {
         }
     }
 
-    const toogleVisiblePwd = ()=>{
+    const toogleVisiblePwd = () => {
         let tar = document.getElementById('pwd1')
-        if(tar.type === 'password')
-        {
+        if (tar.type === 'password') {
             tar.type = 'text'
         }
-        else
-        {
+        else {
             tar.type = 'password'
         }
     }
 
     return (
-        <div className='h-screen'>
+        <div className={loading ? 'h-screen opacity-50' : 'h-screen'}>
+            <div ref={loaderRef} className={loading ? 'h-16 w-16 bg-black absolute left-2/4 top-2/4' : 'hidden'}></div>
             <div className='flex justify-center'>
                 <div className='flex flex-col justify-center' style={{ width: "70vw" }}>
                     <div className='text-3xl text-center p-8'>
@@ -67,7 +94,7 @@ export const Signin = () => {
                     <div className='p-4 border border-slate-200 my-2'>
                         <div>Password</div>
                         <input type="password" id='pwd1' className='w-full' placeholder='password' onChange={(e) => { setPwd(e.target.value) }} />
-                        <input type='checkbox' onClick={toogleVisiblePwd}/> show
+                        <input type='checkbox' onClick={toogleVisiblePwd} /> show
                     </div>
 
                     <button className='p-4 rounded-xl transition-colors text-slate-200 bg-slate-950 hover:text-slate-950 hover:bg-slate-200' onClick={handleSignIn}>Signin</button>
